a54871976f4def875ca546b680be6ae51e33c976
“ulisses85” <lukasz.kuligowski@gmail.com>
2018-08-13 14:05:39 +0100

    [feat4]-home component initialsetup

--- src/features/Home.js
+++ src/features/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 export default class Home extends Component {
  render() {
   return (
-   <div>This is my Home component</div>
+   <div>this is my Home component</div>
   );
  }
 }
