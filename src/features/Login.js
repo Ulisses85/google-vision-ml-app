8dafd768c73b2c945adfe79f13f158eae13d9499
“ulisses85” <lukasz.kuligowski@gmail.com>
2018-08-13 14:06:06 +0100

    [feat5]-Login component initialsetup

--- src/features/Login.js
+++ src/features/Login.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-export default class Login extends Component {
+export default class Home extends Component {
  render() {
   return (
-   <div>This is my Login component</div>
+   <div>this is my Login component</div>
   );
  }
 }
